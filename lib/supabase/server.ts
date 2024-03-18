086cc723c3c82897abdf8015c7eaaae6db2ebda6
AmrMustafa <amrdarwish115@gmail.com>
2024-03-18 04:10:29 +0200

    debug2

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,47 +1,9 @@
-// import { createServerClient, type CookieOptions } from "@supabase/ssr";
-// import { cookies } from "next/headers";
-// import { Database } from "../types/supabase";
-
-// export function supabaseServer() {
-//  const cookieStore = cookies();
-
-//  return createServerClient<Database>(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-//   {
-//    cookies: {
-//     get(name: string) {
-//      return cookieStore.get(name)?.value;
-//     },
-//     set(name: string, value: string, options: CookieOptions) {
-//      try {
-//       cookieStore.set({ name, value, ...options });
-//      } catch (error) {
-//       // The `set` method was called from a Server Component.
-//       // This can be ignored if you have middleware refreshing
-//       // user sessions.
-//      }
-//     },
-//     remove(name: string, options: CookieOptions) {
-//      try {
-//       cookieStore.set({ name, value: "", ...options });
-//      } catch (error) {
-//       // The `delete` method was called from a Server Component.
-//       // This can be ignored if you have middleware refreshing
-//       // user sessions.
-//      }
-//     },
-//    },
-//   }
-//  );
-// }
-
 "use server";
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { Database } from "../types/supabase";
 
-export const supabaseServer = () => {
+export function supabaseServer() {
  const cookieStore = cookies();
 
  return createServerClient<Database>(
@@ -52,7 +14,25 @@ export const supabaseServer = () => {
     get(name: string) {
      return cookieStore.get(name)?.value;
     },
+    set(name: string, value: string, options: CookieOptions) {
+     try {
+      cookieStore.set({ name, value, ...options });
+     } catch (error) {
+      // The `set` method was called from a Server Component.
+      // This can be ignored if you have middleware refreshing
+      // user sessions.
+     }
+    },
+    remove(name: string, options: CookieOptions) {
+     try {
+      cookieStore.set({ name, value: "", ...options });
+     } catch (error) {
+      // The `delete` method was called from a Server Component.
+      // This can be ignored if you have middleware refreshing
+      // user sessions.
+     }
+    },
    },
   }
  );
-};
+}
